ed3bceaad62bdeacca9920dfa0dad684b298ef63
Kyle Moxham <kylemoxham@kmcv.co.uk>
2021-08-01 21:56:42 +0100

    Setup chart

---
+++ src/components/chart.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import { Chart as ChartJs, registerables } from 'chart.js';
+
+ChartJs.register(...registerables);
+
+const Chart = ({ monthlyDataset, cumulativeDataset, labels }) => {
+  const [ chart, setChart ] = useState(null);
+
+
+  useEffect(() => {
+    const config = {
+      type: 'line',
+      data: {
+        labels,
+        datasets: [{
+          label: 'Monthly',
+          data: monthlyDataset,
+          fill: false,
+          borderColor: 'red',
+          tension: 0.1
+        },{
+          label: 'Cumulative',
+          data: cumulativeDataset,
+          fill: false,
+          borderColor: 'blue',
+          tension: 0.1
+        }]
+      },
+      options: {}
+    }
+    if (chart) {
+      chart.data.datasets[0].data = monthlyDataset;
+      chart.data.datasets[1].data = cumulativeDataset;
+      chart.update();
+    } else {
+      setChart(new ChartJs(
+        document.getElementById('myChart'),
+        config
+      ))
+    }
+    // eslint-disable-next-line
+  }, [monthlyDataset, cumulativeDataset])
+
+  return (
+    <>
+      <canvas id='myChart' />
+    </>
+
+  );
+};
+
+export default Chart;
